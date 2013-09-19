ae315ea70233d0349f2f3cb9063e706a999aa40e
PteranodonJohn <gonthier.john@gmail.com>
2013-09-19 09:32:12 -0700

    Added HtmlHelperExtensions.cs

---
+++ ConsultantProfiles/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConsultantProfiles.Extensions
+{
+  public class HtmlHelperExtensions
+  {
+  }
+}
